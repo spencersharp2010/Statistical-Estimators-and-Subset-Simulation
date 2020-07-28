f7dec1cd09074f5f8fdf33d93ffdac4dc068639f
ge73ver <ge73ver@mytum.de>
2020-07-29 00:21:30 +0200

    working on problem 1. Finished MC subtask - working on MCMC

---
+++ problem1_MC.m
@@ -0,0 +1,47 @@
+clear;
+
+%% setup
+% define N such that coeff of variation is roughly 5%
+N = 2e4;
+
+% limit state function, derived by hand
+g = @(X1,X2) -10*X1.^2 + 2*X1.*X2 + 2*X2.^2;
+
+rng(1);
+
+% inverted CDFs, used to generate samples from each distribution
+cdf_x1_inv = @(u) 20./(2-u);
+cdf_x2_inv = @(u) 20*sqrt(u);
+
+max_iter = 1000;
+%% run Monte Carlo simulation 1000 times
+for i=1:max_iter
+    u1 = rand(N,1);
+    u2 = rand(N,1);
+    
+    x1 = cdf_x1_inv(u1);
+    x2 = cdf_x2_inv(u2);
+    
+    g_eval = g(x1,x2);
+    
+    I = g_eval >= 0;
+    Q(i) = sum(I)/N;
+    Var_Pr(i)=var(I)/N;
+    
+    delta_Q(i)=sqrt(Var_Pr(i))/Q(i);
+end
+
+
+% below are two methods of estimating the coefficient of variation
+c_v1 = mean(delta_Q)
+c_v2 = std(Q) / mean(Q)
+
+% average probability of failure
+Q_avg = mean(Q)
+
+% since CI = 0.95, a = 1-0.95 = 0.05
+a=0.05;
+
+% calculate confidence intervals
+ci_lower=Q_avg-norminv(1-a/2).*sqrt(mean(Var_Pr))
+ci_upper=Q_avg+norminv(1-a/2).*sqrt(mean(Var_Pr))
---
+++ problem1_MCMC.m
@@ -0,0 +1,104 @@
+clear;
+
+g = @(X) -10*X(1).^2 + 2*X(1).*X(2) + 2*X(2).^2;
+jointpdf = @(x) (10<=x(1) & x(1)<=20).*(0<=x(2) & x(2)<=20) ...
+    .* (x(2)/(10*x(1)^2));
+
+pdf_x1 = @(x1) 20/(x1^2);
+pdf_x2 = @(x2) x2/200;
+
+% parameter sigma
+%sigma=[0.1 0.5 1 5 10 50];
+sigma = 5;
+
+% Length of burn-in period
+burn=1000;
+
+% Sample size
+N=2e4;
+% Q=zeros(size(sigma,2),1);
+% Var_Q=zeros(size(sigma,2),1);
+% delta_Q=zeros(size(sigma,2),1);
+% 
+% f1=figure; hold on; xlabel('Lag','FontSize',14); ylabel('Sample autocorrelation','FontSize',14);
+% set(f1, 'Units', 'normalized', 'Position', [0.2, 0.3, 0.5, 0.5])
+
+% Target density parameters
+% mu_log=0;
+% si_log=1;
+
+% Seed value
+rng(1,'twister');
+
+
+%for i=1:length(sigma)
+    
+x=zeros(burn+N+1,2);
+x(1,:)=[15,10];
+
+% Candidate sample
+sigma_mat = eye(2)*sigma;
+x_cand = mvnrnd([0,0],sigma_mat,N+burn);
+% Uniform sample
+u=rand(burn+N,1);
+
+% MCMC algorithm
+for j=1:burn+N
+    
+    % Generate proposal state of the Markov chain
+    y=x(j,:)+x_cand(j,:);
+    
+    % Acceptance probability
+    %alpha=min(1,lognpdf(y,mu_log,si_log)/lognpdf(x(j),mu_log,si_log));
+    eval = jointpdf(y)/jointpdf(x(j,:));
+    alpha = min(1, eval );
+    % Accept or reject sample
+    if u(j)<=alpha
+        x(j+1,:)=y;
+    else
+        x(j+1,:)=x(j,:);
+    end
+   
+end
+
+% Discard the samples from the burn-in period
+x_fin=x(burn+2:end,:);
+
+for k=1:N
+    g_eval(k) = g(x_fin(k,:));
+end
+
+I = g_eval >= 0;
+Q = sum(I)/N;
+Var_Pr=var(I)/N;
+
+
+% figure;
+% plot(x_fin(:,1))
+% title('x1')
+% figure;
+% plot(x_fin(:,2))
+% title('x2');
+% figure;
+% scatter(x_fin(:,1),x_fin(:,2))
+% Mean of the estimator
+%Q=mean(x_fin,);
+
+
+% Coefficient of variation of the estimator
+% estimate the coefficient of variation of Q
+nlags = 200;
+acf_Y = autocorr(g_eval,nlags);
+gamma = 2*sum((1-(1:nlags)/(length(g_eval)))'.*acf_Y(2:end));
+Var_Q = var(g_eval)/N*(1+gamma);
+delta_Q=sqrt(Var_Q)/Q
+% 
+% plot(acf_x)
+% legendInfo{i} = ['\sigma = ' num2str(sigma(i))];
+% 
+% %end
+% 
+% lgd=legend(legendInfo,'Location','eastoutside','Orientation','vertical','FontSize',14);
+% xlim([0 nlags])
+% 
+% delta_Q
